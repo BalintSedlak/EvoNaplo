c6c2a66fa57df9cfa3683bc06217b1afde6ab857
Balint Sedlak <balint.sedlak@evosoft.com>
2021-12-21 20:12:12 +0100

    Add Guard Clauses

---
+++ EvoNaplo.Common/GuardClauses/GuardClauseExtensions.cs
@@ -0,0 +1,248 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EvoNaplo.Common.GuardClauses
+{
+    /// <summary>
+    /// A collection of common guard clauses, implemented as extensions.
+    /// </summary>
+    public static class GuardClauseExtensions
+    {
+        /// <summary>
+        /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not null.</returns>
+        public static T Null<T>(this IGuardClause guardClause, T input, string parameterName, string? message = null)
+        {
+            if (input is null)
+            {
+                if (string.IsNullOrEmpty(message))
+                {
+                    throw new ArgumentNullException(parameterName);
+                }
+                throw new ArgumentNullException(message, (Exception?)null);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+        /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is an empty string.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not an empty string or null.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static string NullOrEmpty(this IGuardClause guardClause, string? input, string parameterName, string? message = null)
+        {
+            Guard.Against.Null(input, parameterName);
+            if (input == string.Empty)
+            {
+                throw new ArgumentException(message ?? $"Required input {parameterName} was empty.", parameterName);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+        /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is an empty guid.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not an empty guid or null.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static Guid NullOrEmpty(this IGuardClause guardClause, Guid? input, string parameterName, string? message = null)
+        {
+            Guard.Against.Null(input, parameterName);
+            if (input == Guid.Empty)
+            {
+                throw new ArgumentException(message ?? $"Required input {parameterName} was empty.", parameterName);
+            }
+
+            return input.Value;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+        /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is an empty enumerable.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not an empty enumerable or null.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static IEnumerable<T> NullOrEmpty<T>(this IGuardClause guardClause, IEnumerable<T>? input, string parameterName, string? message = null)
+        {
+            Guard.Against.Null(input, parameterName);
+            if (!input.Any())
+            {
+                throw new ArgumentException(message ?? $"Required input {parameterName} was empty.", parameterName);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+        /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is an empty or white space string.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not an empty or whitespace string.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static string NullOrWhiteSpace(this IGuardClause guardClause, string? input, string parameterName, string? message = null)
+        {
+            Guard.Against.NullOrEmpty(input, parameterName);
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentException(message ?? $"Required input {parameterName} was empty.", parameterName);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is zero.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not zero.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        private static T Zero<T>(this IGuardClause guardClause, T input, string parameterName, string? message = null) where T : struct
+        {
+            if (EqualityComparer<T>.Default.Equals(input, default(T)))
+            {
+                throw new ArgumentException(message ?? $"Required input {parameterName} cannot be zero.", parameterName);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException" /> if <paramref name="input"/> is negative.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not negative.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        private static T Negative<T>(this IGuardClause guardClause, T input, string parameterName, string? message = null) where T : struct, IComparable
+        {
+            if (input.CompareTo(default(T)) < 0)
+            {
+                throw new ArgumentException(message ?? $"Required input {parameterName} cannot be negative.", parameterName);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException"/> if <paramref name="input"/> is negative or zero. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not negative or zero.</returns>
+        private static T NegativeOrZero<T>(this IGuardClause guardClause, T input, string parameterName, string? message = null) where T : struct, IComparable
+        {
+            if (input.CompareTo(default(T)) <= 0)
+            {
+                throw new ArgumentException(message ?? $"Required input {parameterName} cannot be zero or negative.", parameterName);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not default for that type.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static T Default<T>(this IGuardClause guardClause, T input, string parameterName, string? message = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(input, default(T)!) || input is null)
+            {
+                throw new ArgumentException(message ?? $"Parameter [{parameterName}] is default value for type {typeof(T).Name}", parameterName);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException" /> if  <paramref name="input"/> doesn't match the <paramref name="regexPattern"/>.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="regexPattern"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static string InvalidFormat(this IGuardClause guardClause, string input, string parameterName, string regexPattern, string? message = null)
+        {
+            if (input != Regex.Match(input, regexPattern).Value)
+            {
+                throw new ArgumentException(message ?? $"Input {parameterName} was not in required format", parameterName);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentOutOfRangeException" /> if  any <paramref name="input"/>'s item is less than <paramref name="rangeFrom"/> or greater than <paramref name="rangeTo"/>.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="rangeFrom"></param>
+        /// <param name="rangeTo"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if any item is not out of range.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static IEnumerable<T> OutOfRange<T>(this IGuardClause guardClause, IEnumerable<T> input, string parameterName, T rangeFrom, T rangeTo, string? message = null) where T : IComparable, IComparable<T>
+        {
+            if (rangeFrom.CompareTo(rangeTo) > 0)
+            {
+                throw new ArgumentException(message ?? $"{nameof(rangeFrom)} should be less or equal than {nameof(rangeTo)}");
+            }
+
+            if (input.Any(x => x.CompareTo(rangeFrom) < 0 || x.CompareTo(rangeTo) > 0))
+            {
+                if (string.IsNullOrEmpty(message))
+                {
+                    throw new ArgumentOutOfRangeException(parameterName, message ?? $"Input {parameterName} had out of range item(s)");
+                }
+                throw new ArgumentOutOfRangeException(message, (Exception?)null);
+            }
+
+            return input;
+        }
+    }
+}
---
+++ EvoNaplo.Common/GuardClauses/IGuardClause.cs
@@ -0,0 +1,9 @@
+﻿namespace EvoNaplo.Common.GuardClauses
+{
+    /// <summary>
+    /// Simple interface to provide a generic mechanism to build guard clause extension methods from.
+    /// </summary>
+    public interface IGuardClause
+    {
+    }
+}
