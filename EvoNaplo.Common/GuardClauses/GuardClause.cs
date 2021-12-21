2bef4fd6b5de758a1da9baf5ef2cf21324743e14
Balint Sedlak <balint.sedlak@evosoft.com>
2021-12-21 20:12:12 +0100

    Add Guard Clauses

---
+++ EvoNaplo.Common/GuardClauses/GuardClause.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoNaplo.Common.GuardClauses
+{
+    /// <summary>
+    /// An entry point to a set of Guard Clauses defined as extension methods on IGuardClause.
+    /// </summary>
+    public class Guard : IGuardClause
+    {
+        /// <summary>
+        /// An entry point to a set of Guard Clauses.
+        /// </summary>
+        public static IGuardClause Against { get; private set; }
+
+        private Guard() 
+        {
+            Against = new Guard();
+        }
+    }
+}
