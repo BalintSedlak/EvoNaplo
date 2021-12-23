bbd72ce76841abf9f1cea431bd9f0f32d93bbddc
Balint Sedlak <balint.sedlak@evosoft.com>
2021-12-23 12:19:39 +0100

    Add UserDomain.Models

---
+++ EvoNaplo.UserDomain/Models/UserHelper.cs
@@ -0,0 +1,19 @@
+﻿using EvoNaplo.Common.Models.DTO;
+
+namespace EvoNaplo.UserDomain.Models
+{
+    //TODO: Replace this with property mapper
+    internal static class UserHelper
+    {
+        public static UserDTO ConvertUserToUserDTO(User user)
+        {
+            return new UserDTO
+            {
+                Id = user.Id,
+                Name = $"{user.FirstName} {user.LastName}",
+                Email = user.Email,
+                PhoneNumber = !string.IsNullOrEmpty(user.PhoneNumber) ? user.PhoneNumber : "No data"
+            };
+        }
+    }
+}
