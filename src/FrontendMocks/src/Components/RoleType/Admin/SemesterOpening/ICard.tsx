250d171d2bda206f98f3e9d4dcaf4addfb555638
Balint Sedlak <balint.sedlak@evosoft.com>
2022-03-25 22:58:46 +0100

    Remove prototype project

--- src/FrontendMocks/src/Components/RoleType/Admin/SemesterOpening/ICard.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-
-interface ICard {
-    draggableId: number;
-    title: string;
-    index: number
-  }
-
-  export default ICard;
--- src/FrontendMocks/src/Components/RoleType/Admin/SemesterOpening/ICardContent.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-
-interface ICardContent {
-    id: number;
-    title: string;
-  }
-
-  export default ICardContent;
