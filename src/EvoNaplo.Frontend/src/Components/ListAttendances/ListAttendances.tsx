b18bf93e6983769728060d60881d16121306aca5
Baumel Márton <marcibaumel@gmail.com>
2022-05-11 20:38:03 +0200

    Minor changes

--- src/EvoNaplo.Frontend/src/Components/ListAttendances/ListAttendances.tsx
+++ src/EvoNaplo.Frontend/src/Components/ListAttendances/ListAttendances.tsx
@@ -1,150 +1,101 @@
 import React from 'react'
 import styled from 'styled-components';
-import { useTable, Column, useSortBy } from "react-table";
 import { Dropdown } from 'react-bootstrap';
 
 
-const columns: Column<Data>[] = [
-  {
-    Header: "Name",
-    accessor: "name"
-  },
-  {
-    Header: "Hét 1- 2022.05.06.", accessor: 'attendance',
-  },
-  {
-    Header: "Hét 1- 2022.05.06.", accessor: 'attendance2',
-  },
 
 
-
-];
-
-interface Data {
-  name: string;
-  attendance: string;
-  attendance2: string;
+interface AttendanceType {
+  Semester: string;
+  Project: string;
+  Name: string;
+  Status: string;
 }
 
-const data: Data[] = 
-[ 
-  {
-    name: "Béla",
-    attendance: "Online",
-    attendance2: "Online"
-  },
-  {
-    name: "István",
-    attendance: "Offline",
-    attendance2: "Offline"
-  }
+interface Data {
+  Week: string;
+  Details: AttendanceType[];
+}
 
 
+const data: Data[] = [{
+  Week: "Hét 1 2022.01.03. - 2022.01.09.",
+  Details: [
+    {
+      Semester: "2022/1",
+      Project: "EvoNaplo",
+      Name: "Kis Béla",
+      Status: "Online"
+    },
+    {
+      Semester: "2022/1",
+      Project: "EvoNaplo",
+      Name: "Nagy Béla",
+      Status: "Online"
+    }
+  ]
+},
+{
+  Week: "Hét 2 2022.01.10. - 2022.01.17.",
+  Details: [
+    {
+      Semester: "2022/1",
+      Project: "EvoNaplo",
+      Name: "Kis Béla",
+      Status: "Online"
+    },
+    {
+      Semester: "2022/1",
+      Project: "EvoNaplo",
+      Name: "Nagy Béla",
+      Status: "Online"
+    }
+  ]
+},
 ];
 
+
+
+
 const Styles = styled.div`
-  padding: 1rem;
-
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
+td, th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
 `
 
 
 const ListAttendances = () => {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow
-  } = useTable<Data>({ columns, data }, useSortBy)
-
 
   return (
     <>
-      <Dropdown className='mx-auto my-2'>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Semester
-        </Dropdown.Toggle>
 
-        <Dropdown.Menu>
-          <Dropdown.Item >2020</Dropdown.Item>
-          <Dropdown.Item >2021</Dropdown.Item>
-          <Dropdown.Item >2022</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
 
-      <Dropdown className='mx-auto my-2'>
-        <Dropdown.Toggle variant='info' id="dropdown-basic">
-          Project
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item>EvoNaplo</Dropdown.Item>
-          <Dropdown.Item>EvoProject</Dropdown.Item>
-          <Dropdown.Item>EvoRPG</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
       <Styles>
-        <table {...getTableProps()}>
+        <tbody>
           <thead>
-            {headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                    {console.log(column.getSortByToggleProps())}
-                    {column.render("Header")}
-                    <span>
-                      {" "}
-                      {column.isSorted
-                        ? column.isSortedDesc
-                          ? " 🔽"
-                          : " 🔼"
-                        : ""}{" "}
-                    </span>
-                  </th>
-                ))}
-              </tr>
-            ))}
+          <tr>
+            <th>Name</th>
+            {data.map((items, i) => (
+              <th key={i}>{items.Week}</th>
+          ))}
+          </tr>
           </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row, i) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()} onClick={() => console.log(row.original)}>
-                  {row.cells.map(cell => {
-                    return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+
+         
+
+        </tbody>
+
       </Styles>
     </>
   )
 }
-
+/*{data.map((items, i) => (
+            <th key={i}>
+              {console.log(i)}
+              {items.Attendances.Students.map((items, index => {
+                 <tr>{item[index].Name}</tr>
+              }))}
+            </th>
+          ))}*/
 export default ListAttendances
