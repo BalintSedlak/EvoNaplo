b90ecc1c584cdc14cdb7d2f77fb46f49ab4f4a74
Baumel Márton <marcibaumel@gmail.com>
2022-06-06 15:19:40 +0200

    Form view done

--- src/EvoNaplo.Frontend/src/Components/Registration/Form/RegistrationForm.tsx
+++ src/EvoNaplo.Frontend/src/Components/Registration/Form/RegistrationForm.tsx
@@ -10,15 +10,18 @@ interface IFormInput {
 }
 
 export const RegistrationForm = (props) => {
+
   const {
     register,
     handleSubmit,
+    watch,
+    getValues,
     formState: { errors }
   } = useForm<IFormInput>();
 
   const onSubmit = (data: IFormInput) => {
     alert(JSON.stringify(data));
-  }; // your form submit function which will invoke after successful validation
+  }; 
 
 
   return (
@@ -31,21 +34,104 @@ export const RegistrationForm = (props) => {
           {...register("firstName", {
             required: true,
             maxLength: 20,
-            pattern: /^[A-Za-z]+$/i
+            minLength: 3,
+            pattern: /^[A-Za-záéúőóüö]+$/i
           })}
         />
-        {errors?.firstName?.type === "required" && <p>This field is required</p>}
+        {errors?.firstName?.type === "required" && <p className={classes.ErrorParagraph}>This field is required</p>}
+        {errors?.firstName?.type === "minLength" && (
+          <p className={classes.ErrorParagraph}>First name cannot be less than 3 characters</p>
+        )}
         {errors?.firstName?.type === "maxLength" && (
-          <p>First name cannot exceed 20 characters</p>
+          <p className={classes.ErrorParagraph}>First name cannot exceed 20 characters</p>
         )}
         {errors?.firstName?.type === "pattern" && (
-          <p>Alphabetical characters only</p>
+          <p className={classes.ErrorParagraph}>Alphabetical characters only</p>
         )}
-        <label>Laste Name</label>
-        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+
+
+        <label>Last Name</label>
+        <input
+          type="text"
+          {...register("lastName", {
+            required: true,
+            maxLength: 20,
+            minLength: 3,
+            pattern: /^[A-Za-záéúőóüö]+$/i
+          })}
+        />
+        {errors?.lastName?.type === "required" && <p className={classes.ErrorParagraph}>This field is required</p>}
+        {errors?.lastName?.type === "minLength" && (
+          <p className={classes.ErrorParagraph}>Last name cannot be less than 3 characters</p>
+        )}
+        {errors?.lastName?.type === "maxLength" && (
+          <p className={classes.ErrorParagraph}>Last name cannot exceed 20 characters</p>
+        )}
         {errors?.lastName?.type === "pattern" && (
-          <p>Alphabetical characters only</p>
+          <p className={classes.ErrorParagraph}>Alphabetical characters only</p>
         )}
+
+        <label>Email</label>
+        <input
+          type="text"
+          {...register("email", {
+            required: true,
+            maxLength: 20,
+            minLength: 3,
+            pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i
+            //email already in database
+          })}
+        />
+        {errors?.email?.type === "required" && <p className={classes.ErrorParagraph}>This field is required</p>}
+        {errors?.email?.type === "minLength" && (
+          <p className={classes.ErrorParagraph}>Email cannot be less than 3 characters</p>
+        )}
+        {errors?.email?.type === "maxLength" && (
+          <p className={classes.ErrorParagraph}>Email cannot exceed 20 characters</p>
+        )}
+        {errors?.email?.type === "pattern" && (
+          <p className={classes.ErrorParagraph}>Not good email address</p>
+        )}
+
+        <label>Password</label>
+        <input
+          type="password"
+          {...register("password", {
+            required: true,
+            maxLength: 20,
+            minLength: 3
+          })}
+        />
+        {errors?.password?.type === "required" && <p className={classes.ErrorParagraph}>This field is required</p>}
+        {errors?.password?.type === "minLength" && (
+          <p className={classes.ErrorParagraph}>Password cannot be less than 3 characters</p>
+        )}
+        {errors?.password?.type === "maxLength" && (
+          <p className={classes.ErrorParagraph}>Password cannot exceed 20 characters</p>
+        )}
+
+        <label>Re-Password</label>
+        <input
+          type="password"
+          {...register("password2", {
+            required: true,
+            maxLength: 20,
+            minLength: 3,
+
+          })}
+        />
+        {errors?.password2?.type === "required" && <p className={classes.ErrorParagraph}>This field is required</p>}
+        {errors?.password2?.type === "minLength" && (
+          <p className={classes.ErrorParagraph}>Password cannot be less than 3 characters</p>
+        )}
+        {errors?.password?.type === "maxLength" && (
+          <p className={classes.ErrorParagraph}>Password cannot exceed 20 characters</p>
+        )}
+        {watch("password2") !== watch("password") &&
+          getValues("password2") ? (
+          <p className={classes.ErrorParagraph}>Passwords not match</p>
+        ) : null}
+
 
         <input type="submit" />
       </form>
