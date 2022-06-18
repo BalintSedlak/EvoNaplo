8b284625ccefd5ed29717ac3c69241d59be9e71e
Baumel Márton <marcibaumel@gmail.com>
2022-06-18 17:03:45 +0200

    Organized

--- src/EvoNaplo.Frontend/src/Components/StudentProfile/StudentProfile.tsx
+++ src/EvoNaplo.Frontend/src/Components/StudentProfile/StudentProfile.tsx
@@ -4,44 +4,48 @@ import ISession from '../../ISession';
 import { UnauthorizedModal } from '../UI/UnauthorizedModal';
 import { EditStudentProfile } from './EditStudentProfile/EditStudentProfile';
 import { ViewStudentProfile } from './ViewStudentProfile/ViewStudentProfile';
+import classes from './StudentProfile.module.css'
 
+//https://localhost:3000/Components/StudentProfile/StudentProfile
+//props: student id
 export const StudentProfile = ({ session }: { session: ISession }) => {
 
   const [editStudentProfile, setEditStudentProfile] = useState('1');
-  
+
 
   if (session.id > 0) {
 
     const buttonElements = [
-      {name: 'View', value: '1'},
-      {name: 'Edit', value: '2'}
+      { name: 'View', value: '1' },
+      { name: 'Edit', value: '2' }
     ]
 
-    const toggleEditProfil = () => {
-
-    }
-
-    
+    //TODO: fetch the student data from backend with the given id
 
     return (
       <>
-       <ButtonGroup>
-        {buttonElements.map((radio, idx) => (
-          <ToggleButton
-            key={idx}
-            id={`radio-${idx}`}
-            type="radio"
-            variant={idx % 2 ? 'outline-success' : 'outline-danger'}
-            name="radio"
-            value={radio.value}
-            checked={editStudentProfile === radio.value}
-            onChange={(e) => setEditStudentProfile(e.currentTarget.value)}
-          >
-            {radio.name}
-          </ToggleButton>
-        ))}
-      </ButtonGroup>
-        {editStudentProfile==='1' ? <ViewStudentProfile/> : <EditStudentProfile/>}
+        <div>
+          <ButtonGroup className={classes.toggleButtonPosition}>
+            {buttonElements.map((radio, idx) => (
+              <ToggleButton
+                key={idx}
+                id={`radio-${idx}`}
+                type="radio"
+                variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                name="radio"
+                value={radio.value}
+                checked={editStudentProfile === radio.value}
+                onChange={(e) => setEditStudentProfile(e.currentTarget.value)}
+              >
+                {radio.name}
+              </ToggleButton>
+            ))}
+          </ButtonGroup>
+        </div>
+        <div>
+          {/*szebb lenne ha magán a komponensen belűl változna a dolog, egyelőre legyen szétszedve a 2 mert egyszerűbb most még úgy megcsinálni*/}
+          {editStudentProfile === '1' ? <ViewStudentProfile /> : <EditStudentProfile />}
+        </div>
       </>
     )
   }
--- src/EvoNaplo.Frontend/src/Components/StudentProfile/ViewStudentProfile/ViewStudentProfile.tsx
+++ src/EvoNaplo.Frontend/src/Components/StudentProfile/ViewStudentProfile/ViewStudentProfile.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Card, ProgressBar } from 'react-bootstrap';
+import Scholarship from './Scholarship';
+import './UserPageView.css'
+
 
 export const ViewStudentProfile = () => {
-  return (
-    <div>ViewStudentProfile</div>
-  )
+    const student =
+    {
+        fullname: "Teszt Benő",
+        studies: "2020.09.03. - 2023.06.10.",
+        technologies: "java, javascript",
+        attendance: 80,
+        mentors: ["Helló Pál, ", "Végre Sikerült, ", "Pénztáros Géza"],
+        teams: ["Első csopi", "G2", "Ausztria csopi"],
+        scholarship: "Nem jelentkezett",
+        email: "teveklub@freemail.hu",
+        phonenumber: "+36363636363",
+        fbgroup: true,
+        internship: false,
+        comment: ["Név Pál", "Nagyon tudja, hogy tudja a dolgát."]
+
+    }
+
+    return (
+        <div className="DivCard">
+            <tr><th>Név</th></tr>
+            <tr>{student?.fullname}</tr>
+            <tr><th>Tanulmányok</th></tr>
+            <tr>{student?.studies}</tr>
+            <tr><th>Technológiák</th></tr>
+            <tr>{student?.technologies}</tr>
+            <tr><th>Jelenlét</th></tr>
+            <tr>{<ProgressBar now={student?.attendance} label={`${student?.attendance}%`} />}</tr>
+            <tr><th>Mentorok</th></tr>
+            <tr>{student?.mentors.map((i) => { return (<li>{i}</li>) })}</tr>
+            <tr><th>Csapatok</th></tr>
+            <tr>{student?.teams.map((i) => { return (<li>{i}</li>) })}</tr>
+            <tr><th>Ösztöndíj</th></tr>
+            {<Scholarship data={student?.scholarship}></Scholarship>}
+            <tr><th>Email</th></tr>
+            <tr>{student?.email}</tr>
+            <tr><th>Telefonszám</th></tr>
+            <tr>{student?.phonenumber}</tr>
+            <tr><th>Facebook csoport</th></tr>
+            <tr>{<input type="checkbox" checked={student?.fbgroup} readOnly={true}></input>}</tr>
+            <tr><th>Szakmai Gyakorlat</th></tr>
+            <tr>{<input type="checkbox" checked={student?.internship} readOnly={true}></input>}</tr>
+            <tr><th>Komment</th></tr>
+            <tr><Card border="success" style={{ width: '18rem' }}>
+                <Card.Header>{student?.comment[0]}</Card.Header>
+                <Card.Body>
+                    <Card.Title>Success Card Title</Card.Title>
+                    <Card.Text>
+                        {student?.comment[1]}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+                <br /></tr>
+        </div>
+    );
 }
