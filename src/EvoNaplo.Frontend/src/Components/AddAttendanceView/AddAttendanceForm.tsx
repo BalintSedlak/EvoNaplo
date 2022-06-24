f347fe95dc64eff248a18b96d6780519b0a787d0
Baumel Márton <marcibaumel@gmail.com>
2022-06-24 09:51:05 +0200

    Update AddAttendanceForm.tsx

--- src/EvoNaplo.Frontend/src/Components/AddAttendanceView/AddAttendanceForm.tsx
+++ src/EvoNaplo.Frontend/src/Components/AddAttendanceView/AddAttendanceForm.tsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { StudentInput } from "./StudentInput";
 
-export const AddAttendanceForm = (SemesterData) => {
+const DUMMY_DATA = [
+  {
+    projectId: 1,
+    students: ["Kis Béla", "Nagy Béla", "Közepes Béla"],
+  },
+  {
+    projectId: 2,
+    students: ["Kis Béla", "Nagy Béla"],
+  },
+];
+
+const WEEK = ["1. Hét", "2. Hét", "3. Hét"];
+
+export const AddAttendanceForm = (props) => {
+  const [selectedProject, setSelectedProject] = useState("");
+  const [selectedWeek, setSelectedWeek] = useState("");
+  const [projectDetails, setProjectDetails] = useState([]);
+
+  useEffect(() => {
+    setProjectDetails(
+      DUMMY_DATA.filter((x) => x.projectId.toString() === selectedProject)
+    );
+  }, [selectedProject]);
+
+  const handleProjectChange = (e) => {
+    console.log(e.target.value);
+    setSelectedProject(e.target.value);
+  };
+
+  const handleWeekChange = (e) => {
+    console.log(e.target.value);
+    setSelectedWeek(e.target.value);
+  }
+
   return (
-    <div>AddAttendanceForm</div>
-  )
-}
+    <>
+      <div className="m-5">
+        <Form.Select
+          aria-label="Default select example"
+          onChange={(e) => handleProjectChange(e)}
+        >
+          <option>Open this select menu</option>
+          {props.ActualProjects.map((data) => (
+            <option key={data.id} value={data.id}>
+              {data.project}
+            </option>
+          ))}
+        </Form.Select>
+
+        <Form.Select className="mt-2" onChange={(e) => handleWeekChange(e)}>
+        <option>Open to select the week</option>
+            {WEEK.map(x => <option>{x}</option>)}
+        </Form.Select>
+      </div>
+
+      <div>
+        {projectDetails.length > 0 && (
+          <div>
+            {projectDetails.map((x) =>
+              x.students.map((x) => <StudentInput studentName={x} project={selectedProject} week={selectedWeek}/>)
+            )}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                margin: "1rem",
+              }}
+            >
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
