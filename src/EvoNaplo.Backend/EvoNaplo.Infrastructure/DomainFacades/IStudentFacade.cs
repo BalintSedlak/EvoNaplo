36a8bf9a759682e1ef39e5f5a7b2501a5ed8e8b4
Baumel Márton <marcibaumel@gmail.com>
2022-07-06 14:05:49 +0200

    WIP: student backend

---
+++ src/EvoNaplo.Backend/EvoNaplo.Infrastructure/DomainFacades/IStudentFacade.cs
@@ -0,0 +1,15 @@
+﻿using EvoNaplo.Infrastructure.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoNaplo.Infrastructure.DomainFacades
+{
+    public interface IStudentFacade
+    {
+        List<StudentEntity> GetAllStudent();
+        StudentEntity EditStudent(StudentEntity student);
+    }
+}
---
+++ src/EvoNaplo.Backend/EvoNaplo.WebApp/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using EvoNaplo.Infrastructure.DomainFacades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvoNaplo.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentFacade _studentFacade;
+
+        public StudentController(IStudentFacade studentFacade)
+        {
+            _studentFacade = studentFacade;
+        }
+    }
+}
